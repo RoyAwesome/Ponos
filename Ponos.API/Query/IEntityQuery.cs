ba75ebdaea8db63968c8ba1e0f327add29476517
Garrett <kylegar@kylegar.net>
2021-03-09 00:02:53 -0800

    API for Entity Queries

---
+++ Ponos.API/Query/IEntityQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponos.API.Query
+{
+    public interface IEntityQuery
+    {
+        public IEnumerable<EntityQueryResult> QueryResults
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IEntityQuery<T> where T : struct
+    {
+        public IEnumerable<EntityQueryResult<T>> QueryResults
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IEntityQuery<T, U>
+        where T : struct
+        where U : struct
+    {
+        public IEnumerable<EntityQueryResult<T, U>> QueryResults
+        {
+            get;
+            set;
+        }
+    }
+
+
+
+}
